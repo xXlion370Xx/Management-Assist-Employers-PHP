a6ba215d992b46912831cc9568c14fb03cc34eab
Daniel Roa <lion35528@gmail.com>
2023-01-25 21:16:33 -0500

    update headers

--- config/headers.php
+++ config/headers.php
@@ -4,6 +4,7 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Max-Age: 86400");
 
+
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
 }
